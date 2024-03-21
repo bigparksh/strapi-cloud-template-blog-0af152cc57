2cb442b96d94d6c9a93fa44a2fe16576dc2e14c6
박성화 <bigparksh@gmail.com>
2024-03-21 21:04:16 +0900

    - refreshToken 추가 - SMTP email provide 추가

--- config/plugins.js
+++ config/plugins.js
@@ -5,4 +5,33 @@ module.exports = () => ({
       // See `Config` section.
     },
   },
+  // email: {
+  //   config:{
+  //     provider: 'sendmail',
+  //     providerOptions: {},
+  //     settings: {
+  //       defaultFrom: 'no-reply@tuneit.io',
+  //       defaultReplyTo: 'no-reply@tuneit.io',
+  //     },
+  //   },
+  // }
+  email: {
+    config: {
+      provider: 'strapi-provider-email-smtp',
+      providerOptions: {
+        host: 'smtp.gmail.com', //SMTP Host
+        port: 465, //SMTP Port
+        secure: true,
+        username: 'bigparksh2@gmail.com',
+        password: 'ezax mmag gvxb ymoq',
+        rejectUnauthorized: true,
+        requireTLS: true,
+        connectionTimeout: 1,
+      },
+      settings: {
+        defaultFrom: 'no-reply@tuneit.io',
+        defaultReplyTo: 'no-reply@tuneit.io',
+      },
+    },
+  },
 });
---
+++ src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,28 @@
+module.exports = (plugin) => {
+  plugin.controllers.auth.refreshToken = async (ctx) => {
+    // refresh userself token
+    const newJwt = strapi.plugins['users-permissions'].services.jwt.issue({
+      id: ctx.state.user.id
+    })
+    return { jwt: newJwt }
+
+    // comment out next lines, and refresh the request body's token like {token: 'xxx'}
+    // const { token } = ctx.request.body
+    // const payload = strapi.plugins['users-permissions'].services.jwt.verify(token)
+    // const newJwt = strapi.plugins['users-permissions'].services.jwt.issue({
+    // 	id: payload.id
+    // })
+    // return {jwt: newJwt}
+  }
+
+  plugin.routes['content-api'].routes.push({
+    method: 'POST',
+    path: '/auth/refreshToken',
+    handler: 'auth.refreshToken',
+    config: {
+      prefix: ''
+    }
+  });
+
+  return plugin
+}
